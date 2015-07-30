a58a9b3761da4efb2576c69bacf851cc2b0e8efd
Catabriea Muellerleile <cmuellerleile@Catabrieas-MacBook-Air.local>
2015-07-30 10:46:37 -0500

    employee

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'client/scripts/app.js',
+                //added scripts - now create scripts folder in assets
+                dest: 'server/public/assets/scripts/app.min.js'
+            }
+        },
+        copy: {
+            bootstrap: {
+                expand: true,
+                cwd: "node_modules/",
+                src: [
+                    "bootstrap/dist/css/bootstrap.min.css",
+                    //changed the ^ line of code to access css from node modules root in bootstrap
+                ],
+                "dest": "server/public/vendors/"
+            },
+            jquery: {
+                expand: true,
+                cwd: "node_modules/",
+                src: [
+                    "jquery/dist/jquery.min.js",
+                    //changed the ^ line of code to access css from node modules root in bootstrap
+                ],
+                "dest": "server/public/vendors/"
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['copy', 'uglify']);
---
+++ server/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port',(process.env.PORT || 5000));
+
+app.get('/data-request', function(req, res){
+    //how to handle these requests
+    res.json({message : "Hello"});
+});
+
+app.get('/*', function(req, res){
+    //dropped in * wildcard
+    var file = req.params[0] || "views/index.html";
+    res.sendFile(path.join(__dirname, "./public/", file));
+    // $$$ big money code
+    // what that does is it sets a default file
+});
+
+app.listen(app.get('port'), function(){
+    console.log("Hey! Listening on Port: " + app.get('port'));
+});
+
+//install grunt and grunt-contrib-uglify
